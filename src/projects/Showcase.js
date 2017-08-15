1984356f909baa519507ce9f1ba39bf9650414c7
foerever <sc80@rice.edu>
2017-08-15 06:48:51 -0500

    Added back unique id for finding hidden div for each polaroid to fix modal setting issue

--- src/projects/Showcase.js
+++ src/projects/Showcase.js
@@ -11,6 +11,7 @@ class Showcase extends Component {
         super(props);
         enableUniqueIds(this)
         this.state = {
+            originalId: String(this.nextUniqueId()),
             uniqueId: String(this.nextUniqueId()) 
         }
     }
@@ -19,16 +20,16 @@ class Showcase extends Component {
         const { title, image, description, link } = this.props
         return (
             <div>
-                <div className="hidden_div description">
+                <div className="hidden_div description" id={this.state.originalId}>
                     <h1>{title}</h1>
                     <p>{description}</p>
                 </div>
 
 
                 <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                    <div className="polaroid_container more_info">
-                        <div className="polaroid" id={this.state.uniqueId}>
-                            <div className="polaroid-image">
+                    <div className="polaroid_container" >
+                        <div className="polaroid">
+                            <div className="polaroid-image" id={this.state.uniqueId}>
                                 <img src={image} alt="" height="200px"/>
                             </div>
                             <h3>{title}</h3>
@@ -42,8 +43,8 @@ class Showcase extends Component {
 
     componentDidMount() {
         var modal = new tingle.modal();
-        var testing = "." + this.state.originalId
         var more_information = document.getElementById(this.state.uniqueId);
+        modal.setContent(document.getElementById(this.state.originalId))
         more_information.addEventListener('click', function(){
             modal.open();
         });
