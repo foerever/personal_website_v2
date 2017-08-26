e0272dd77348c9551e93e57ca520e0eeab452fe6
foerever <sc80@rice.edu>
2017-08-26 01:27:29 -0500

    Fixed images disappearing due to erroneous symlink path

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({ template: './src/index.html', filename: 'index.html', inject: 'body' })
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({ template: './client/index.html', filename: 'index.html', inject: 'body' })
 module.exports = {
-  entry: './src/index.js',
+  entry: './client/index.js',
   output: {
     path: path.resolve('dist'),
     filename: 'index_bundle.js'
